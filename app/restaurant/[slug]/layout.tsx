c5cab8ec116a9d3675a9f81ed1fb27a3d11dd612
sh.shadanpoor <sh.shadanpoor@pixaldo.com>
2023-08-22 00:02:07 +0330

    fix header

--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -1,16 +1,18 @@
-import React from "react";
 import Header from "./components/Header";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+export default function RestaurantLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { slug: string };
+}) {
   return (
     <main>
-      <Header />
-
+      <Header name={params.slug} />
       <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
         {children}
       </div>
     </main>
   );
-};
-
-export default Layout;
+}
