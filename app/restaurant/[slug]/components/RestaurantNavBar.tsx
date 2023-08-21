0be64068e1a847adf17857b75bf32232aa5cb1d1
sh.shadanpoor <sh.shadanpoor@pixaldo.com>
2023-08-21 23:59:50 +0330

    set data

--- app/restaurant/[slug]/components/RestaurantNavBar.tsx
+++ app/restaurant/[slug]/components/RestaurantNavBar.tsx
@@ -1,19 +1,14 @@
 import Link from "next/link";
-import React from "react";
 
-const RestaurantNavBar = () => {
+export default function RestaurantNavBar({ slug }: { slug: string }) {
   return (
     <nav className="flex text-reg border-b pb-2">
-      <Link href="/restaurant/milestone" className="mr-7">
-        {" "}
-        Overview{" "}
+      <Link href={`/restaurant/${slug}`} className="mr-7">
+        Overview
       </Link>
-      <Link href="/restaurant/milestone/menu" className="mr-7">
-        {" "}
-        Menu{" "}
+      <Link href={`/restaurant/${slug}/menu`} className="mr-7">
+        Menu
       </Link>
     </nav>
   );
-};
-
-export default RestaurantNavBar;
+}
